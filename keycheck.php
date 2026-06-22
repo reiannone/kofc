9c2f53ea1d2436c92d9f24fcd10338cd1319f55c
reiannone <reiannone@gmail.com>
2026-06-22 12:40:12 -0400

    Remove keycheck utility

--- keycheck.php
+++
@@ -1,5 +0,0 @@
-<?php
-require "C:/xampp/htdocs/kofc/api/config.php";
-$k = ANTHROPIC_API_KEY;
-echo "len=" . strlen($k) . " starts=" . substr($k, 0, 7) . PHP_EOL;
-echo "mock=" . var_export(AI_MOCK, true) . PHP_EOL;
